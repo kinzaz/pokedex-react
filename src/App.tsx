18208320f946e2e406a9f5a514473a3534bedefd
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-04-08 15:18:15 +0300

    search feature by name and type

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,74 @@
-import { useEffect, useRef, useState } from "react";
-import { Pokedex } from "./components/Pokedex";
-import { Pokemon } from "./types/Pokemon";
-import { fetchPokemonList } from "./api/fetchPokemonList";
+import { useEffect, useRef, useState } from 'react';
+import { Pokedex } from './components/Pokedex';
+import { Pokemon } from './types/Pokemon';
+import { fetchPokemonList } from './api/fetchPokemonList';
+import { SearchBar } from './components/SearchBar';
 
 const App = () => {
-  const [modal, setModal] = useState(false);
-  const [pokemonData, setPokemonData] = useState<Pokemon>();
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [pokemonAmount, setPokemonAmount] = useState(9);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
-  const [showPagination, setShowPagination] = useState(true);
-  const [disabledButton, setDisabledButton] = useState(false);
-  const searchBarRef = useRef<HTMLDivElement>(null);
+	const [modal, setModal] = useState(false);
+	const [pokemonData, setPokemonData] = useState<Pokemon>();
+	const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+	const [pokemonAmount, setPokemonAmount] = useState(9);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [page, setPage] = useState(1);
+	const [showPagination, setShowPagination] = useState(true);
+	const [disabledButton, setDisabledButton] = useState(false);
+	const searchBarRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      setPokemonList(await fetchPokemonList(1));
-      setLoading(false);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			setLoading(true);
+			setPokemonList(await fetchPokemonList(1));
+			setLoading(false);
+		})();
+	}, []);
 
-  useEffect(() => {
-    const html = document.documentElement;
+	useEffect(() => {
+		const html = document.documentElement;
 
-    modal
-      ? (html.style.overflow = "hidden")
-      : (html.style.overflow = "initial");
-  }, [modal]);
+		modal
+			? (html.style.overflow = 'hidden')
+			: (html.style.overflow = 'initial');
+	}, [modal]);
 
-  useEffect(() => {
-    setError(false);
-  }, [pokemonList]);
+	useEffect(() => {
+		setError(false);
+	}, [pokemonList]);
 
-  return (
-    <>
-      <Pokedex
-        setModal={setModal}
-        setPokemonData={setPokemonData}
-        pokemonList={pokemonList}
-        setPokemonList={setPokemonList}
-        pokemonAmount={pokemonAmount}
-        setPokemonAmount={setPokemonAmount}
-        error={error}
-        loading={loading}
-        setLoading={setLoading}
-        page={page}
-        setPage={setPage}
-        showPagination={showPagination}
-        setShowPagination={setShowPagination}
-        searchBarRef={searchBarRef}
-        disabledButton={disabledButton}
-      />
-    </>
-  );
+	return (
+		<>
+			<SearchBar
+				setPokemonList={setPokemonList}
+				pokemonAmount={pokemonAmount}
+				setPokemonAmount={setPokemonAmount}
+				setError={setError}
+				setLoading={setLoading}
+				setPage={setPage}
+				setShowPagination={setShowPagination}
+				disabledButton={disabledButton}
+				setDisabledButton={setDisabledButton}
+				searchBarRef={searchBarRef}
+			/>
+			<Pokedex
+				setModal={setModal}
+				setPokemonData={setPokemonData}
+				pokemonList={pokemonList}
+				setPokemonList={setPokemonList}
+				pokemonAmount={pokemonAmount}
+				setPokemonAmount={setPokemonAmount}
+				error={error}
+				loading={loading}
+				setLoading={setLoading}
+				page={page}
+				setPage={setPage}
+				showPagination={showPagination}
+				setShowPagination={setShowPagination}
+				searchBarRef={searchBarRef}
+				disabledButton={disabledButton}
+			/>
+		</>
+	);
 };
 
 export default App;
